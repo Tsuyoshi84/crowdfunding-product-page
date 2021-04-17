1484717890916d2dd239ecee485bb53013ddbe47
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2021-04-17 14:44:46 +0900

    Add postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = (ctx) => ({
+  map: ctx.env === 'development' ? ctx.map : false,
+  plugins: {
+    'postcss-nesting': {},
+    cssnano: ctx.env === 'production' ? {} : false,
+  },
+})
