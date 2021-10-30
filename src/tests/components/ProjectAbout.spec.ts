1d6841555581b7121cf9e5d6cb033e27a98eb39c
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2021-10-30 15:53:54 +0900

    Add ProjectAbout test

---
+++ src/tests/components/ProjectAbout.spec.ts
@@ -0,0 +1,34 @@
+import { render, fireEvent } from '@testing-library/vue'
+import ProjectAbout from '~/components/ProjectAbout.vue'
+import { Project } from '../../models/project'
+
+const project: Project = {
+  id: 1,
+  name: 'An awesome project',
+  description: 'This is an awesome project',
+  detail: 'This is an awesome project',
+  bookmarked: false,
+  targetAmount: 10000,
+  currentAmount: 2000,
+  numOfBuckers: 2,
+  daysLeft: 10,
+  rewards: [
+    {
+      id: 1,
+      name: 'Pen',
+      detail: 'An awesome pen',
+      left: 2,
+      pledge: 3,
+    },
+  ],
+}
+
+test('increments value on click', async () => {
+  const { getByText } = render(ProjectAbout, {
+    props: {
+      project,
+    },
+  })
+
+  getByText(project.detail)
+})
