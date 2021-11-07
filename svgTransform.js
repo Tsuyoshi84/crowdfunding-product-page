943cc1a5cbebf663bee86c39a300d521bc2e470c
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2021-11-07 17:52:28 +0900

    Add transformer

--- svgTransform.js
+++ svgTransform.js
@@ -1,5 +1,25 @@
+const { parse, compileTemplate } = require('@vue/compiler-sfc')
+const { transform } = require('@babel/core')
+const babelPreset = require('@babel/preset-env')
+
+function getFileNameFromPath(path) {
+  return path.split('/').pop()
+}
+
+function replaceFileExtension(path, ext) {
+  return path.replace(/\.[^/.]+$/, ext)
+}
+
 module.exports = {
-  process(src) {
-    return 'module.exports = {};'
+  process(content, filePath) {
+    const filename = replaceFileExtension(getFileNameFromPath(filePath), '.vue')
+    const source = `<template>${content}</template>`
+
+    const parsed = parse(source, { filename })
+    const template = compileTemplate({
+      source: parsed.descriptor.template.content,
+    }).code
+
+    return transform(template, { presets: [babelPreset] })
   },
 }
