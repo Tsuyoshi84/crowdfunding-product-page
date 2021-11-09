6d886e17e2a2332bcc7a39cfac3689e0cfc96903
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2021-11-09 23:18:43 +0900

    Set mobile style

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <project />
-  <img src="@/assets/design/active-states-default.jpg" alt="" />
-  <img src="@/assets/design/mobile-design.jpg" alt="" />
+  <!-- <img src="@/assets/design/active-states-default.jpg" alt="" />
+  <img src="@/assets/design/mobile-design.jpg" alt="" /> -->
 </template>
 
 <script lang="ts" setup>
@@ -15,6 +15,7 @@ import Project from './components/Project.vue'
   --color-disabled: hsl(0deg 0% 44%);
   --color-disabled-light-1: hsl(0deg 0% 54%);
   --color-disabled-light-2: hsl(0deg 0% 69%);
+  --color-border: hsl(0deg 0% 80%);
 
   --spacing-2: 0.5rem;
   --spacing-4: 1rem;
@@ -22,12 +23,13 @@ import Project from './components/Project.vue'
   --spacing-12: 3rem;
   --spacing-16: 4rem;
 
-  --font-size-small: 0.75rem;
+  --font-size-small: 0.8rem;
   --font-size-medium: 1rem;
-  --font-size-large: 1.25rem;
-  --font-size-xlarge: 1.5rem;
-  --font-size-xxlarge: 1.75rem;
-  --font-size-xxxlarge: 2rem;
+  --font-size-large: 1.2rem;
+  --font-size-xlarge: 1.4rem;
+  --font-size-xxlarge: 1.6rem;
+  --font-size-xxxlarge: 1.8rem;
+  --font-size-xxxxlarge: 2rem;
 
   --font-weight-light: 300;
   --font-weight-normal: 400;
--- src/components/ProjectStatus.vue
+++ src/components/ProjectStatus.vue
@@ -12,16 +12,18 @@ defineProps({
 </script>
 
 <template>
-  <section>
-    <div>
+  <section class="info-container">
+    <div class="sub-info-container">
       <div class="main">${{ formatNumber(project.currentAmount) }}</div>
       <div class="sub">of ${{ formatNumber(project.targetAmount) }} backed</div>
     </div>
-    <div>
+    <div class="border"></div>
+    <div class="sub-info-container">
       <div class="main">{{ formatNumber(project.numOfBuckers) }}</div>
       <div class="sub">total backers</div>
     </div>
-    <div>
+    <div class="border"></div>
+    <div class="sub-info-container">
       <div class="main">{{ project.daysLeft }}</div>
       <div class="sub">days left</div>
     </div>
@@ -29,13 +31,37 @@ defineProps({
 </template>
 
 <style scoped lang="postcss">
+.info-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1.5rem;
+}
+.sub-info-container {
+  width: 100%;
+  display: flex;
+  gap: 0.5rem;
+  flex-direction: column;
+  justify-content: center;
+}
 .main {
-  font-size: var(--font-size-xxlarge);
+  font-size: var(--font-size-xxxxlarge);
   font-weight: var(--font-weight-bold);
+  display: flex;
+  justify-content: center;
 }
 .sub {
-  font-size: var(--font-size-medium);
+  font-size: var(--font-size-small);
   font-weight: var(--font-weight-light);
+  letter-spacing: 0.05rem;
   color: var(--font-color-sub);
+  display: flex;
+  justify-content: center;
+}
+.border {
+  width: 5rem;
+  height: 1px;
+  background-color: var(--color-border);
 }
 </style>
