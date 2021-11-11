123bad61c501cc40a23eb3ac54e3c73be4c5a7b1
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2021-11-11 14:01:11 +0900

    Add logo image

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
+  <header>
+    <img class="logo" src="@/assets/images/logo.svg" alt="crowdfund" />
+  </header>
   <main>
-    <header></header>
     <project />
   </main>
 </template>
@@ -11,11 +13,20 @@ import Project from './components/Project.vue'
 
 <style lang="postcss">
 header {
-  background: #ff0000;
-  background-image: url('@/assets/images/image-hero-mobile.jpg');
+  background-image: linear-gradient(
+      to bottom,
+      hsla(0deg, 0%, 0%, 0.52),
+      hsla(0deg, 0%, 100%, 0)
+    ),
+    url('@/assets/images/image-hero-mobile.jpg');
   background-size: cover;
   height: 19rem;
   border-block-end: 1px solid #eaeaea;
+
+  & > .logo {
+    margin: var(--spacing-6) 0 0 var(--spacing-6);
+    object-fit: contain;
+  }
 }
 
 @media (min-width: 62em) {
