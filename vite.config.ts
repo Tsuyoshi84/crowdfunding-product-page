dbf3da5bda35e42045c645047c5308c403a7c54a
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2021-11-11 13:38:08 +0900

    Uninstall vite-svg-loader

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +9,5 @@ export default defineConfig({
       { find: 'components', replacement: '/src/components' },
     ],
   },
-  plugins: [vue(), svgLoader()],
+  plugins: [vue()],
 })
