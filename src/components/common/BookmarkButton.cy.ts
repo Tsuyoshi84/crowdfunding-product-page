f00d966d0bf68fecf554193d065b5c09a37f620d
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2021-11-28 20:39:57 +0900

    Add example

--- src/components/common/BookmarkButton.cy.ts
+++ src/components/common/BookmarkButton.cy.ts
@@ -1,20 +1,18 @@
 import { mount } from '@cypress/vue'
 import { it, describe, cy } from 'local-cypress'
 import BookmarkButton from './BookmarkButton.vue'
+import BookmarkButtonExample from './BookmarkButton.ex.vue'
 import '@/assets/styles/main.css'
 import '@/assets/styles/font.css'
 
 describe('BookmarkButton', () => {
-  const bookmarkedList = [true, false]
   const viewportList: Cypress.ViewportPreset[] = ['iphone-x', 'macbook-13']
 
   for (const viewport of viewportList) {
-    for (const bookmarked of bookmarkedList) {
-      it(`viewportList=${viewport}, bookmarked=${bookmarked}`, () => {
-        cy.viewport(viewport)
-        mount(BookmarkButton, { props: { bookmarked } })
-      })
-    }
+    it(`viewportList=${viewport}`, () => {
+      cy.viewport(viewport)
+      mount(BookmarkButtonExample)
+    })
   }
 
   it('shows button', () => {
