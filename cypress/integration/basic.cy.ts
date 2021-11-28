044498932229d1a41a215641bc681f4a1fbadc80
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2021-11-28 21:17:46 +0900

    Modify test

--- cypress/integration/basic.cy.ts
+++ cypress/integration/basic.cy.ts
@@ -10,13 +10,13 @@ context('Basic', () => {
 
     cy.contains('Mastercraft').should('exist')
 
-    // cy.get('#input')
-    //   .type('Vitesse{Enter}')
-    //   .url()
-    //   .should('eq', 'http://localhost:3333/hi/Vitesse')
+    cy.get('[data-cy=back-project]')
+      .click()
+      .contains('Back this project')
+      .should('exist')
 
-    // cy.contains('[Default Layout]').should('exist')
+    cy.get('[data-cy=reward-basic-info]').first().click()
 
-    // cy.get('.btn').click().url().should('eq', 'http://localhost:3333/')
+    cy.get('[data-cy=input-form]').contains('Enter your pledge').should('exist')
   })
 })
