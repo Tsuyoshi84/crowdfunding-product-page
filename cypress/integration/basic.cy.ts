a473088d099a186812db8321682b5e5434f3cb61
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2021-11-30 12:13:46 +0900

    Add toggle bookmark button scenario

--- cypress/integration/basic.cy.ts
+++ cypress/integration/basic.cy.ts
@@ -5,6 +5,17 @@ context('Basic', () => {
     cy.visit('/')
   })
 
+  it('toggle bookmark button', () => {
+    cy.url().should('eq', 'http://localhost:3000/')
+
+    cy.get('[data-cy=bookmark-button]')
+      .should('exist')
+      .click()
+      .contains('Bookmarked')
+      .click()
+      .contains('Bookmark')
+  })
+
   it('basic nav', () => {
     cy.url().should('eq', 'http://localhost:3000/')
 
