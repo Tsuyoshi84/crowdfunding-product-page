0c7ffa33b085b6c5210989ddf2a8f821aaa0cc1d
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2022-09-06 10:10:28 +0900

    Change PrimaryButton test

--- src/components/common/PrimaryButton.cy.ts
+++ src/components/common/PrimaryButton.cy.ts
@@ -2,8 +2,32 @@ import PrimaryButton from './PrimaryButton.vue'
 import '@/assets/styles/main.css'
 import '@/assets/styles/font.css'
 
+type A = InstanceType<typeof PrimaryButton>
+
 describe('<PrimaryButton>', () => {
+  const label = 'Click me'
+  const disabled = false
+  const type = 'button'
+
+  const slots = { default: () => label }
+  const props = { disabled, type }
+  const options = { slots, props }
+
   it('renders', () => {
-    cy.mount(PrimaryButton, { slots: { default: () => 'Button' } })
+    cy.mount(PrimaryButton, options)
+  })
+
+  it('shows label', () => {
+    cy.mount(PrimaryButton, options).get('button').should('have.text', label)
+  })
+
+  it('emmits when clicked', () => {
+    const clickSpy = cy.spy().as('clickSpy')
+
+    cy.mount(PrimaryButton, { slots, props: { ...props, onClick: clickSpy } })
+      .get('button')
+      .click()
+
+    cy.get('@clickSpy').should('have.been.calledOnce')
   })
 })
