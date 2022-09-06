139f417d944041be8d7b62758be74b553c24819f
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2022-09-07 00:24:11 +0900

    Update PrimaryButton test

--- src/components/common/PrimaryButton.cy.ts
+++ src/components/common/PrimaryButton.cy.ts
@@ -2,28 +2,42 @@ import PrimaryButton from './PrimaryButton.vue'
 
 describe('<PrimaryButton>', () => {
   const label = 'Click me'
-  const disabled = false
-  const type = 'button'
-
   const slots = { default: () => label }
-  const props = { disabled, type }
-  const options = { slots, props }
 
-  it('renders', () => {
-    cy.mount(PrimaryButton, options)
+  it('should mount', () => {
+    cy.mount(PrimaryButton, { slots })
   })
 
-  it('shows label', () => {
-    cy.mount(PrimaryButton, options).get('button').should('have.text', label)
+  it('should show label', () => {
+    cy.mount(PrimaryButton, { slots }).get('button').should('have.text', label)
   })
 
-  it('emmits when clicked', () => {
+  it('should emmit when clicked', () => {
     const clickSpy = cy.spy().as('clickSpy')
 
-    cy.mount(PrimaryButton, { slots, props: { ...props, onClick: clickSpy } })
+    cy.mount(PrimaryButton, { slots, props: { onClick: clickSpy } })
       .get('button')
       .click()
 
     cy.get('@clickSpy').should('have.been.calledOnce')
+  })
+
+  it('should have button type as default', () => {
+    cy.mount(PrimaryButton, { slots })
+      .get('button')
+      .should('have.attr', 'type', 'button')
+  })
+
+  it('should have specified type', () => {
+    const type = 'submit'
+    cy.mount(PrimaryButton, { slots, props: { type } })
+      .get('button')
+      .should('have.attr', 'type', type)
+  })
+
+  it('should be disabled', () => {
+    cy.mount(PrimaryButton, { slots, props: { disabled: true } })
+      .get('button')
+      .should('be.disabled')
   })
 })
