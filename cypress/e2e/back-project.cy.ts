c2c29be8469408d3442f694f876ed8a0d94694be
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2022-09-09 17:35:23 +0900

    Split e2e test

---
+++ cypress/e2e/back-project.cy.ts
@@ -0,0 +1,58 @@
+import { Project } from '@/models/project'
+
+describe('Back project', () => {
+  let project: Project
+
+  beforeEach(() => {
+    cy.visit('/')
+
+    cy.fixture<Project>('project').then((p) => {
+      project = p
+    })
+  })
+
+  it('should show modals after backing', () => {
+    cy.screenshot('01-project-page', { capture: 'viewport', overwrite: true })
+
+    cy.getBySel('project-name')
+      .should('be.visible')
+      .should('contain.text', project.name!)
+
+    cy.get('[data-test=project-description]')
+      .should('be.visible')
+      .should('contain.text', project.description!)
+
+    cy.getBySel('back-project')
+      .should('be.visible')
+      .should('contain.text', 'Back this project')
+      .click()
+    cy.screenshot('02-show-project-modal', {
+      capture: 'viewport',
+      overwrite: true,
+    })
+
+    cy.getBySel('project-modal').should('be.visible')
+    cy.getBySel('input-form').should('not.exist')
+
+    cy.getBySel('reward-basic-info').eq(1).click()
+
+    cy.getBySel('input-form')
+      .should('contain.text', 'Enter your pledge')
+      .should('be.visible')
+
+    cy.getBySel('pledge-input')
+      .should('have.value', 25)
+      .type('{backspace}{backspace}')
+      .type('1')
+    cy.getBySel('submit-button').contains('Continue').should('be.disabled')
+
+    cy.getBySel('pledge-input').type('{backspace}').type('100')
+    cy.getBySel('submit-button').should('be.enabled').click()
+
+    cy.screenshot('03-pledge-success', { capture: 'viewport', overwrite: true })
+    cy.getBySel('project-modal').should('not.be.visible')
+    cy.getBySel('complete-modal').should('be.visible')
+    cy.getBySel('complete-modal-close-button').should('be.visible').click()
+    cy.getBySel('complete-modal').should('not.be.visible')
+  })
+})
