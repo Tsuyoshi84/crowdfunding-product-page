e00c61f6d436946f1466ea174ce93ac10d8d0a5d
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2022-09-09 17:56:43 +0900

    Fix component tests

--- src/components/ProjectSummary.cy.ts
+++ src/components/ProjectSummary.cy.ts
@@ -2,18 +2,21 @@ import { Project } from '@/models/project'
 import ProjectSummary from './ProjectSummary.vue'
 
 describe('<ProjectSummary>', () => {
+  let project: Project
+
   beforeEach(() => {
-    cy.fixture<Project>('project').then((project) => {
+    cy.fixture<Project>('project').then((p) => {
+      project = p
       cy.mount(ProjectSummary, { props: { project } })
     })
   })
 
   it('shows texts', () => {
     cy.getBySel('project-name')
-      .should('contain.text', 'An awesome project')
+      .should('contain.text', project.name)
       .should('be.visible')
     cy.getBySel('project-description')
-      .should('contain.text', 'This is an awesome project')
+      .should('contain.text', project.description)
       .should('be.visible')
   })
 })
