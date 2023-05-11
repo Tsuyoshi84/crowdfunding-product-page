6fba57c6ecb3570c70a93f035526c5323cadb5c9
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2023-05-12 07:58:05 +0900

    Fix StyleLint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,6 +3,7 @@ module.exports = {
 		'stylelint-config-standard',
 		'stylelint-config-recommended-vue',
 		'stylelint-prettier/recommended',
+		'stylelint-config-prettier',
 		'stylelint-config-idiomatic-order',
 	],
 	plugins: ['stylelint-use-logical-spec'],
