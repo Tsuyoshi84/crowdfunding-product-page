aec5e9eb5a533faab8ad80e378fae479a316da05
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2025-07-20 13:17:03 +0900

    chore: Update Biome (#635)
    * Update Biome
    
    * Fix
    
    * Apply automatic changes
    
    ---------
    
    Co-authored-by: Tsuyoshi84 <11254699+Tsuyoshi84@users.noreply.github.com>

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,7 @@
+/** biome-ignore-all lint/complexity/noBannedTypes: No need to check */
+/** biome-ignore-all lint/suspicious/noExplicitAny: No need to check */
 declare module '*.vue' {
 	import type { DefineComponent } from 'vue'
-	// biome-ignore lint:
 	const component: DefineComponent<{}, {}, any>
 	export default component
 }
